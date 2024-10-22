97148660c84e94d2b3d60f7ebac259eb1897588d
Guobamao <1972212424@qq.com>
2024-10-23 00:36:37 +0800

    feat(utils): 添加全局请求工具模块
    - 创建 request.js 文件，实现统一请求管理
    - 配置 axios 基本选项和拦截器
    - 处理请求和响应过程中的各种情况
    - 集成 Element UI 的消息提示功能
    - 添加用户登录状态管理

---
+++ src/utils/request.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import { userStore } from '@/store';
+axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
+
+// 创建axios实例，并设置基本配置
+const service = axios.create({
+    baseURL: 'http://192.168.137.1:8081',
+    withCredentials: true,
+    timeout: 10000
+});
+
+// 请求拦截器
+service.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+// 响应拦截器
+service.interceptors.response.use(
+    response => {
+        const { code, message, data } = response.data;
+        if (code === 200) {
+            return data;
+        } else if (code === 401) {
+            ElMessage.warning("登录过期，请重新登录");
+            userStore().logOut().then(() => {
+                setTimeout(() => {
+                    location.href = '/'
+                }, 1000)
+            })
+            return Promise.reject('登录过期，请重新登录');
+        } else {
+            ElMessage.warning(message || '请求失败');
+            return Promise.reject(new Error(message || 'Error'));
+        }
+    },
+    error => {
+        console.log('响应失败', error);
+        ElMessage.error("发生了一些错误，请联系管理员");
+        return Promise.reject(error);
+    }
+);
+
+export default service
