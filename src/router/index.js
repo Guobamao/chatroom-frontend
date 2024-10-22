63c68aa3db32d126f45dab38f37f010315a5d274
Guobamao <1972212424@qq.com>
2024-10-23 00:36:45 +0800

    feat(router): 添加路由守卫和嵌套路由
    - 新增路由守卫，根据用户登录状态进行权限控制
    - 实现嵌套路由，优化应用结构
    - 添加登录和注册页面路由
    - 设置聊天页面路由

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { userStore } from "@/store";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'welcome',
+            component: () => import('@/views/WelcomeView.vue'),
+            children: [
+                {
+                    path: '',
+                    name: 'welcome-login',
+                    component: () => import('@/views/welcome/LoginPage.vue')
+                },{
+                    path: 'register',
+                    name: 'welcome-register',
+                    component: () => import('@/views/welcome/RegisterPage.vue')
+                },
+            ]
+        } , {
+            path: '/index',
+            name: 'index',
+            component: () => import('@/views/IndexView.vue'),
+            children: [
+                {
+                    path: '*',
+                    name: 'chat',
+                    component: () => import('@/views/chat/ChatPage.vue')
+                }
+            ]
+        }
+    ]
+})
+
+router.beforeEach((to,from,next) =>{
+    // 获取store中的用户信息
+    const store = userStore()
+
+    // 判断是否登录，登录为false，未登录为true
+    const isAuthorized = store.id ? true : false
+    
+    if(to.name.startsWith('welcome-') && isAuthorized){
+        next('/index')
+    }else if(to.fullPath.startsWith('/index') && !isAuthorized){
+        next('/')
+    }else {
+        next()
+    }
+})
+export default router
